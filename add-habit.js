fcb17e9c7127ada49e4318e401a8861397e2dd57
annieruddhhh <anirudhsingla5686@gmail.com>
2026-05-07 14:02:01 +0530

    .......3

--- add-habit.js
+++ add-habit.js
@@ -26,7 +26,7 @@ async function addHabit() {
     }
 
     // send data to flask
-    const response = await fetch("http://127.0.0.1:5000", {
+    const response = await fetch("http://127.0.0.1:5000/habits", {
 
         method: "POST",
 
--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-const API = "http://127.0.0.1:5000"
+const API = "http://127.0.0.1:5000/habits"
 
 async function loadHabits() {
     const container = document.getElementById("habits-container")
